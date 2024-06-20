5c9a5c479fa9332060d0757d404f585a5bac1fa9
llipengda <lipengda2@outlook.com>
2024-06-20 17:04:59 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
+import { ThemeProvider, createTheme } from '@mui/material'
+
 import App from './App.tsx'
 import './index.css'
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#b71c1c'
+    },
+    secondary: {
+      main: '#ffffff'
+    }
+  }
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 )
